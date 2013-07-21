2147962dac1ee46faeb2287d69ea2a5a15d73fb0
mogli123 <onevibes@web.de>
2013-07-21 08:29:05 +0200

    added more configurable in TechniHDSetup

--- usr/lib/enigma2/python/Plugins/Extensions/TechniHDSetup/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TechniHDSetup/plugin.py
@@ -4,7 +4,7 @@
 #    Original Source Coded by Vali (c)2011 (FlexControl)
 #
 #######################################################################
-
+                                                            
 
 from Plugins.Plugin import PluginDescriptor
 from Screens.Screen import Screen
@@ -12,7 +12,7 @@ from Screens.MessageBox import MessageBox
 from Screens.Standby import TryQuitMainloop
 from Components.ActionMap import ActionMap
 from Components.Pixmap import Pixmap
-from Components.config import config, ConfigSubsection, getConfigListEntry, ConfigSelection, ConfigInteger
+from Components.config import config, ConfigSubsection, getConfigListEntry, ConfigSelection, ConfigInteger, ConfigYesNo
 from Components.ConfigList import ConfigListScreen
 from Tools.Directories import fileExists
 from skin import parseColor
@@ -21,7 +21,8 @@ from os import system
 
 config.technihd = ConfigSubsection()
 config.technihd.SecondInfobar = ConfigSelection(default="NowAndNextInfo", choices = [("TunerAndCryptInfos", _("TunerState, Crypt, SNR,...")),("NowAndNextInfo", _("Now and Next EventInfo"))])
-
+config.technihd.Infobar = ConfigSelection(default="NextEventInfo", choices = [("BouquetInfo", _("BouquetInfo")),("NextEventInfo", _("Next EventInfo"))])
+config.technihd.InfobarSatPos = ConfigYesNo(default=False)
 
 class TechniHDSetup(ConfigListScreen, Screen):
 	skin = """
@@ -30,7 +31,7 @@ class TechniHDSetup(ConfigListScreen, Screen):
 			<ePixmap alphatest="blend" pixmap="TechniHD/buttons/green.png" position="322,528" size="40,40" zPosition="2" />
 			<eLabel font="Regular;20" halign="left" position="100,529" size="180,26" text="Cancel" transparent="1" />
 			<eLabel font="Regular;20" halign="left" position="354,529" size="180,26" text="Save" transparent="1" />
-			<widget itemHeight="28" name="config" position="51,339" scrollbarMode="showOnDemand" size="492,165" font="Regular; 15" />
+			<widget itemHeight="30" name="config" position="51,339" scrollbarMode="showOnDemand" size="492,165" font="Regular; 16" />
 		        <eLabel position="48,30" size="492,253" zPosition="1" backgroundColor="grey" />
                         <eLabel position="50,32" size="488,249" zPosition="2" backgroundColor="dblau" />
                         <widget name="preview" zPosition="3" position="60,41" size="469,230" alphatest="blend" />
@@ -44,6 +45,8 @@ class TechniHDSetup(ConfigListScreen, Screen):
 		self["preview"] = Pixmap()
 		list = []
 		list.append(getConfigListEntry(_("Show SecondInfobar with:"), config.technihd.SecondInfobar))
+		list.append(getConfigListEntry(_("Show Infobar with:"), config.technihd.Infobar))
+		list.append(getConfigListEntry(_("Show Infobar with OrbitalPosition:"), config.technihd.InfobarSatPos))
 		ConfigListScreen.__init__(self, list, on_change = self.UpdateComponents)
 		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"], 
 									{
@@ -73,6 +76,18 @@ class TechniHDSetup(ConfigListScreen, Screen):
 				elif 'title="Second InfoBar Normal"' in x:	
                                         if config.technihd.SecondInfobar.value == "NowAndNextInfo":
 						x = '<screen name="SecondInfoBarDisabled" title="Second InfoBar Normal" position="0,0" zPosition="11" size="1280,720" flags="wfNoBorder" backgroundColor="transparent">\n'
+				elif 'EventNextInformation' in x:
+					if config.technihd.Infobar.value == "BouquetInfo":
+						x = '<!-- BouquetInformation!! do not change this line --><widget source="session.CurrentService" render="Label" position="255,608" size="760,27" font="hd;22" halign="left" backgroundColor="hblau" transparent="1"><convert type="BouquetInfo">Bouquet</convert></widget>\n'
+				elif 'BouquetInformation' in x:	
+                                        if config.technihd.Infobar.value == "NextEventInfo":
+						x = '<!-- EventNowAndNextInformation!! do not change this line --><widget source="session.Event_Next" render="Label" position="241,607" size="91,25" font="Regular2; 20" halign="center" backgroundColor="hblau" transparent="1"><convert type="EventTime">StartTime</convert><convert type="ClockToText">Format:%H:%M</convert></widget><widget source="session.Event_Next" render="Label" position="319,607" size="86,25" font="Regular2; 20" halign="center" backgroundColor="hblau" transparent="1"><convert type="EventTime">EndTime</convert><convert type="ClockToText">Format:- %H:%M</convert></widget><widget source="session.Event_Next" render="Label" position="414,608" size="600,25" font="hd;22" halign="left" backgroundColor="hblau" transparent="1"><convert type="EventName">Name</convert></widget>\n'
+				elif 'ChannelNameInformation' in x:
+					if config.technihd.InfobarSatPos.value == True:
+						x = '<!-- OrbitPosAndChanNameInformation!! do not change this line --><widget source="session.CurrentService" render="Label" position="254,648" size="91,25" font="Regular2; 20" valign="center" backgroundColor="background" transparent="1"><convert type="ServiceOrbitalPosition">Short</convert></widget><widget source="session.CurrentService" render="Label" position="335,642" size="494,37" font="Regular; 30" valign="center" backgroundColor="background" transparent="1" zPosition="1"><convert type="ServiceName">Name</convert></widget>\n'
+				elif 'OrbitPosAndChanNameInformation' in x:
+					if config.technihd.InfobarSatPos.value == False:
+						x = '<!-- ChannelNameInformation!! do not change this line --><widget source="session.CurrentService" render="Label" position="255,642" size="572,37" font="Regular; 30" valign="center" backgroundColor="background" transparent="1" zPosition="1"><convert type="ServiceName">Name</convert></widget>\n'
 				skFile.close()
                                 skin_lines.append(x)
 			xFile = open(self.datei, "w")
